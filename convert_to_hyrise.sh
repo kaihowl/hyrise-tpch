ccea2d01257449e1fc2a59ab12074a46b2b69dcb
kaihowl <kai@hoewelmeyer.eu>
2014-01-21 17:40:45 +0100

    Converting all tables.

--- convert_to_hyrise.sh
+++ convert_to_hyrise.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-if [ "$#" -eq 0 ];then
-  echo 'Please supply the table name.'
-else
+TPCHTABLES=( customer lineitem nation orders part partsupp region supplier )
+for TABLE in "${TPCHTABLES[@]}"
+do
+  echo $TABLE :
   echo Concatenating header and data...
-  cat headers/$1.head rawdata/$1.tbl > $1_tmp.tbl
+  cat headers/$TABLE.head rawdata/$TABLE.tbl > $TABLE_tmp.tbl
   echo Stripping trailing pipes...
-  sed "s/|$//g" $1_tmp.tbl > hyrise/$1.tbl
+  sed "s/|$//g" $TABLE_tmp.tbl > hyrise/$TABLE.tbl
   echo Cleaning up...
-  rm $1_tmp.tbl
+  rm $TABLE_tmp.tbl
   echo Creating the load query file...
-  sed "s/{{tablename}}/$1/g" load_template.json > queries/load_$1.json
+  sed "s/{{tablename}}/$TABLE/g" load_template.json > queries/load_$TABLE.json
   echo Done!
-fi
+done
